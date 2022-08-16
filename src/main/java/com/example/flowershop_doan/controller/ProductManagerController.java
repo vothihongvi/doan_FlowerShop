736d1e0391da1e2711f425f654359dc7aeeda343
phanvodinh <91683188+phanvodinh@users.noreply.github.com>
2022-08-16 23:58:08 +0700

    xem xóa và thêm chưa xong

---
+++ src/main/java/com/example/flowershop_doan/controller/ProductManagerController.java
@@ -0,0 +1,36 @@
+package com.example.flowershop_doan.admin;
+
+import com.example.flowershop_doan.bean.Category;
+import com.example.flowershop_doan.bean.Product;
+import com.example.flowershop_doan.dao.ProductDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ProductManagerController", value = "/admin/product-manager")
+public class ProductManagerController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ProductDao dao = new ProductDao();
+        List<Product> list = dao.getAllProduct();
+        List<Category> categoryList = dao.getAllCategory();
+        request.setAttribute("listP", list);
+        request.setAttribute("listCC", categoryList);
+        request.getRequestDispatcher("product-manager.jsp").forward(request, response);
+        //response.sendRedirect("admin/product-manager.jsp");
+
+//        for(Category category: categoryList){
+//            System.out.println(category.toString());
+//        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
